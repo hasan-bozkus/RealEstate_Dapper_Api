abdc3186217ced830d891082492d534efe119fa4
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-09-24 20:29:51 +0300

    ders sonu 10

---
+++ RealEstate_Dapper_UI/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/Layout/_HeaderViewComponentParital.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.ViewComponents.Layout
+{
+    public class _HeaderViewComponentParital : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
