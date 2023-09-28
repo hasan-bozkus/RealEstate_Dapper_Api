4de457209b1f51e04cdeb9f785220d63c9821c7b
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-09-28 10:45:09 +0300

    ders sonu 13

---
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Realestate_Dapper_UI.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string Dİstrict { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultServicesComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
