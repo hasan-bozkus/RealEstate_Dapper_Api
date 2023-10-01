06c2716551edccaf39522392cd2dfff6d8f44b67
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-01 19:55:36 +0300

    ders sonu 16

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Realestate_Dapper_UI.Dtos.WhoWeAreDtos;
 
 namespace Realestate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultWhoWeAreComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultWhoWeAreComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/WhoWeAreDetail");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                ViewBag.title = value.Select(x => x.Title).FirstOrDefault();
+                ViewBag.subTitle = value.Select(x => x.SubTitle).FirstOrDefault();
+                ViewBag.description1 = value.Select(x => x.Description1).FirstOrDefault();
+                ViewBag.description2 = value.Select(x => x.Description2).FirstOrDefault();
+                return View();
+            }
             return View();
         }
     }
