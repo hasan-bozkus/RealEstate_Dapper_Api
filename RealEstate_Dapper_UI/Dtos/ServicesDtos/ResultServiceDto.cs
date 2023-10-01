9ff55cb5716c1721da31aa54e1ed97b3d42209c2
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-01 20:45:15 +0300

    ders sonu 18

---
+++ RealEstate_Dapper_UI/Dtos/ServicesDtos/ResultServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Realestate_Dapper_UI.Dtos.ServicesDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceID { get; set; }
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+    }
+}
