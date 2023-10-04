d9441c1d962153fb0c6b2f7860ba3c9c36052db0
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-04 15:28:32 +0300

    ders sonu 22

---
+++ RealEstate_Dapper_Api/Dtos/SubFeatureDtos/CreateSubFeatureDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.SubFeatureDtos
+{
+    public class CreateSubFeatureDto
+    {
+        public string Icon { get; set; }
+        public string TopTitle { get; set; }
+        public string MainTitle { get; set; }
+        public string Description { get; set; }
+        public string SubTitle { get; set; }
+    }
+}
