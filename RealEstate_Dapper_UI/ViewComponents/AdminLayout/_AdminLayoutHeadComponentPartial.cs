3cb391fc77d617a76e9081d23e81b4c424466b9f
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-05 13:40:05 +0300

    ders sonu 23

---
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutHeadComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
