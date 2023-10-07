81099b0985579b4ff71381a5bcf2f539e59b65be
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-10-07 22:22:10 +0300

    ders sonu 30

---
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Realestate_Dapper_UI.Dtos.EmployeeDtos;
+
+namespace Realestate_Dapper_UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/Employees");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
