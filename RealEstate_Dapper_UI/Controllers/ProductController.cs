570a37fe59e7a0d3482d8d705db33bbf13e9cd9f
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-11-28 13:28:07 +0300

    ders sonu 45: Dashboard hazırlığı

--- RealEstate_Dapper_UI/Controllers/ProductController.cs
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -47,5 +47,27 @@ namespace Realestate_Dapper_UI.Controllers
 
             return View();
         }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToFalse(int Id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/Products/ProductDealOfTheDayStatusChangeToFalse/" + Id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+              return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToTrue(int Id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/Products/ProductDealOfTheDayStatusChangeToTrue/" + Id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+              return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
