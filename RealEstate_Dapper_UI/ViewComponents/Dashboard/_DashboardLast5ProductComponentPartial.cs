bb4cfbac424db3a82a9d2965ab9a767c82117ea4
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-11-30 13:27:40 +0300

    ders sonu 46: Dashboard İşlemleri 1

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardLast5ProductComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast5ProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardStatisticsComponentPartial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardStatisticsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+
+            #region ProductCount
+            var responseMessage1 = await client.GetAsync("https://localhost:44348/api/Statistics/ProductCount");
+            var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.ProductCount = jsonData1;
+            #endregion
+
+            #region EmployeeNameByMaxProductCount
+            var responseMessage2 = await client.GetAsync("https://localhost:44348/api/Statistics/EmployeeNameByMaxProductCount");
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            ViewBag.EmployeeNameByMaxProductCount = jsonData2;
+            #endregion
+
+            #region DifferentCityCount
+            var responseMessage3 = await client.GetAsync("https://localhost:44348/api/Statistics/DifferentCityCount");
+            var jsonData3 = await responseMessage3.Content.ReadAsStringAsync();
+            ViewBag.DifferentCityCount = jsonData3;
+            #endregion
+
+            #region avergeProductPriceByRent
+            var responseMessage4 = await client.GetAsync("https://localhost:44348/api/Statistics/AvergeProductPriceByRent");
+            var jsonData4 = await responseMessage4.Content.ReadAsStringAsync();
+            ViewBag.avergeProductPriceByRent = jsonData4;
+            #endregion
+
+            return View();
+        }
+    }
+}
