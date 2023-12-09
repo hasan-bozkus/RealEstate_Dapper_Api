b63483109b89185cdbefe35171a83e2bdb15fc01
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-12-09 16:59:40 +0300

    ders sonu 49: Dashboardda Son 4 Mesajı Listeleme

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
 using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Repositories.ContactRepositories;
 using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
@@ -29,6 +30,7 @@ namespace RealEstate_Dapper_Api
             builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
             builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
             builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
+            builder.Services.AddTransient<IContactRepository, ContactRepository>();
             builder.Services.AddControllers();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast4ContactListComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Realestate_Dapper_UI.Dtos.ContactDtos;
+
+namespace Realestate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardLast4ContactListComponentPartial : ViewComponent
+    {
+        IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast4ContactListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/Contacts/GetLast4Contact");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<GetLast4ContactResultDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
