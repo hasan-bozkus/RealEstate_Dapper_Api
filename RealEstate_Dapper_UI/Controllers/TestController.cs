68f01ef6ebc53a480a3a5b605e98d70978ca755f
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-01-06 14:19:51 +0300

    ders sonu 52: SignalR Bağlantı İşlemi

---
+++ RealEstate_Dapper_UI/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Realestate_Dapper_UI.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
