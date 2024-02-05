00e4844165b9014f29358a05ea70e3e14ce424a7
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-05 19:44:00 +0300

    ders sonu 56: JWT Token Generator Sınıfı

---
+++ RealEstate_Dapper_Api/Enum/RolesType.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate_Dapper_Api.Enum
+{
+    public enum RolesType
+    {
+        Admin=1,
+        Member=2,
+        Visitor=3,
+        Manager=4,
+        Agent=5
+    }
+}
---
+++ RealEstate_Dapper_Api/Tools/JWTTokenGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealEstate_Dapper_Api.Tools
+{
+    public class JWTTokenGenerator
+    {
+        public static TokenResponseViewModel GenerateToken(GetCheckAppUserViewModel model)
+        {
+            var claims = new List<Claim>();
+            if (!string.IsNullOrWhiteSpace(model.Role))
+                claims.Add(new Claim(ClaimTypes.Role, model.Role));
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, model.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(model.UserName))
+                claims.Add(new Claim("UserName", model.UserName));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+            var signinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience: JwtTokenDefaults.ValidAudience, claims: claims, notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials: signinCredentials);
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            return new TokenResponseViewModel(tokenHandler.WriteToken(token), expireDate);
+        }
+        
+
+    }
+}
