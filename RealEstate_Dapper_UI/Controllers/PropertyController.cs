392ae05b7f51c5e50d2b118cd8f56c3b05708f64
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-05-01 17:13:12 +0300

    ders sonu 72: Single Property Sayfası

---
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Realestate_Dapper_UI.Dtos.ProductDtos;
+
+namespace Realestate_Dapper_UI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public PropertyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44348/api/Products/ProductListWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PropertySingle(int id)
+        {
+            return View();
+        }
+    }
+}
